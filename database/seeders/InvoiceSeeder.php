ddbdb009bc122fbf1e9d1934966fdf847601a5e9
slymackjr <jbnyamasheki@gmail.com>
2025-03-13 04:02:16 +0300

    chore: seeder fixed

--- database/seeders/InvoiceSeeder.php
+++ database/seeders/InvoiceSeeder.php
@@ -24,7 +24,8 @@ class InvoiceSeeder extends Seeder
                 'package_name' => 'Monthly Membership',
                 'discount_percentage' => 10,
                 'start_date' => '2023-09-01',
-                'end_date' => '2023-09-30'
+                'end_date' => '2023-09-30',
+                'member_id' => 1 // You can set this to an actual member ID
             ],
             [
                 'user_name' => 'Alice Johnson',
@@ -37,7 +38,8 @@ class InvoiceSeeder extends Seeder
                 'package_name' => 'Quarterly Membership',
                 'discount_percentage' => 5,
                 'start_date' => '2023-08-01',
-                'end_date' => '2023-10-31'
+                'end_date' => '2023-10-31',
+                'member_id' => 2 // Update member_id
             ],
             [
                 'user_name' => 'Michael Green',
@@ -50,7 +52,8 @@ class InvoiceSeeder extends Seeder
                 'package_name' => 'Annual Membership',
                 'discount_percentage' => 15,
                 'start_date' => '2023-07-01',
-                'end_date' => '2024-06-30'
+                'end_date' => '2024-06-30',
+                'member_id' => 3 // Update member_id
             ],
             [
                 'user_name' => 'Chris Lee',
@@ -63,7 +66,8 @@ class InvoiceSeeder extends Seeder
                 'package_name' => 'Monthly Membership',
                 'discount_percentage' => 10,
                 'start_date' => '2023-10-01',
-                'end_date' => '2023-10-31'
+                'end_date' => '2023-10-31',
+                'member_id' => 4 // Update member_id
             ],
             [
                 'user_name' => 'Emma Brown',
@@ -76,7 +80,8 @@ class InvoiceSeeder extends Seeder
                 'package_name' => 'Bi-Annual Membership',
                 'discount_percentage' => 12,
                 'start_date' => '2023-05-01',
-                'end_date' => '2023-10-31'
+                'end_date' => '2023-10-31',
+                'member_id' => 5 // Update member_id
             ],
             [
                 'user_name' => 'David Kim',
@@ -89,7 +94,8 @@ class InvoiceSeeder extends Seeder
                 'package_name' => 'Weekly Membership',
                 'discount_percentage' => 3,
                 'start_date' => '2023-11-01',
-                'end_date' => '2023-11-07'
+                'end_date' => '2023-11-07',
+                'member_id' => 6 // Update member_id
             ],
             [
                 'user_name' => 'Sophia King',
@@ -102,7 +108,8 @@ class InvoiceSeeder extends Seeder
                 'package_name' => 'Monthly Membership',
                 'discount_percentage' => 8,
                 'start_date' => '2023-06-01',
-                'end_date' => '2023-06-30'
+                'end_date' => '2023-06-30',
+                'member_id' => 7 // Update member_id
             ],
             [
                 'user_name' => 'Lucas Adams',
@@ -115,7 +122,8 @@ class InvoiceSeeder extends Seeder
                 'package_name' => 'Monthly Membership',
                 'discount_percentage' => 0,
                 'start_date' => '2023-07-01',
-                'end_date' => '2023-07-31'
+                'end_date' => '2023-07-31',
+                'member_id' => 8 // Update member_id
             ],
             [
                 'user_name' => 'Grace Thompson',
@@ -128,7 +136,8 @@ class InvoiceSeeder extends Seeder
                 'package_name' => 'Quarterly Membership',
                 'discount_percentage' => 6,
                 'start_date' => '2023-03-01',
-                'end_date' => '2023-05-31'
+                'end_date' => '2023-05-31',
+                'member_id' => 9 // Update member_id
             ],
             [
                 'user_name' => 'Henry Wilson',
@@ -141,7 +150,8 @@ class InvoiceSeeder extends Seeder
                 'package_name' => 'Annual Membership',
                 'discount_percentage' => 20,
                 'start_date' => '2023-12-01',
-                'end_date' => '2024-11-30'
+                'end_date' => '2024-11-30',
+                'member_id' => 10 // Update member_id
             ],
         ];
 
