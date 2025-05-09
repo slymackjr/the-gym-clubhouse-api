1d4a7483545aaa5cc0717e6fbe81bac9c127d6dc
Jofrey Boniphace Nyamasheki <89586004+slymackjr@users.noreply.github.com>
2025-05-10 02:37:43 +0300

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,8 +2,9 @@
 
 # Configuration
 PROJECT_DIR="$(pwd)"
+DB_CONNECTION=mysql
 APP_ENV="production"
-DB_HOST="localhost"
+DB_HOST="127.0.0.1"
 DB_PORT="3306"
 DB_DATABASE="gym-clubhousedb"   
 DB_USERNAME="joker"     
@@ -28,6 +29,7 @@ sed -i \
     -e "s|^DB_HOST=.*|DB_HOST=$DB_HOST|" \
     -e "s|^DB_PORT=.*|DB_PORT=$DB_PORT|" \
     -e "s|^DB_DATABASE=.*|DB_DATABASE=$DB_DATABASE|" \
+    -e "s|^DB_CONNECTION=.*|DB_CONNECTION=$DB_CONNECTION|" \
     -e "s|^DB_USERNAME=.*|DB_USERNAME=$DB_USERNAME|" \
     -e "s|^DB_PASSWORD=.*|DB_PASSWORD=$DB_PASSWORD|" \
     -e "s|^APP_TIMEZONE=.*|APP_TIMEZONE=$APP_TIMEZONE|" \
